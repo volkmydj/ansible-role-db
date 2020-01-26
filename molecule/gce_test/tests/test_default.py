81cb525e87faa57eab9f77e13acd16f2a4c46c80
Aleksey Volkov <aleksey.marantz@gmail.com>
2020-01-27 01:47:28 +0300

    Add modify tests

--- molecule/gce_test/tests/test_default.py
+++ molecule/gce_test/tests/test_default.py
@@ -7,9 +7,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_mongo_running_and_enabled(host):
+    mongo = host.service("mongod")
+    assert mongo.is_running
+    assert mongo.is_enabled
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
+    assert config_file.contains('bindIp: 0.0.0.0')
+    assert config_file.is_file
+
+def test_mongo_listening_port(host):
+    mongo_socket = host.socket("tcp://0.0.0.0:27017")
+    assert mongo_socket.is_listening
+
+# def test_hosts_file(host):
+#     f = host.file('/etc/hosts')
+
+#     assert f.exists
+#     assert f.user == 'root'
+#     assert f.group == 'root'
